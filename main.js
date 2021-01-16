4a6a9be8ec8ff12d84a0661d3061cfe3b4a9f524
Chris Meffley <cmeffley@gmail.com>
2021-01-16 12:08:11 -0500

    added variables, data types and math to file.

--- main.js
+++ main.js
@@ -1,1 +1,49 @@
-console.log('Hello World');
+//console.log("Hello World");
+
+//*******Start Variables*********
+let firstName = 'Beyonce';
+let lastName = 'Knowles';
+
+//console.log(firstName + lastName);
+//console.log(`First Name: ${firstName} Last Name: ${lastName}`);
+
+firstName = 'Chris';
+lastName = 'Meffley';
+
+//console.log(firstName + lastName);
+//console.log(`First Name: ${firstName} Last Name: ${lastName}`);
+
+let favoriteFood = 'tacos';
+//console.log(favoriteFood);
+
+favoriteFood = 'Gumbo';
+//console.log(favoriteFood);
+//*******End Variables*****************
+
+//**** Start Data Types ******
+const dog = 'bark';
+//console.log(typeof dog);
+
+const answer = 33;
+const negative = -14;
+//console.log(typeof answer, typeof negative);
+
+const iLoveJavascript = true;
+//console.log(typeof iLoveJavascript);
+
+//*****END DATA TYPES******
+
+//*****Start MATH******
+//console.log(Math.pow(2, 2));//4
+//console.log(Math.pow(3, 2));//9
+//console.log(Math.pow(3, 3));//27
+
+// console.log(Math.round(6.5));//7
+// console.log(Math.round(6.45)); //6
+// console.log(Math.floor(6.9999)); //6
+// console.log(Math.ceil(6.000001)); //7
+
+const cat = 'kitty';
+//console.log(cat.length); //5
+
+//*****END MATH******
