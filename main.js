a8c9d6efc2befad8fdc84b7352f507966368d3f5
Chris Meffley <cmeffley@gmail.com>
2021-01-16 12:58:12 -0500

    added functions with arguments and calls

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-console.log('Hello World');
+//console.log('Hello World');
+
+
+//function sayHello() {
+ // console.log('Hello!');
+//}
+
+
+//const sayHello = function() {
+  //console.log('Hello!');
+//}
+
+// function sayHello = () => {
+//   console.log('Hello!');
+// }
+
+// sayHello();
+
+// *****Functions with Arguments*****
+
+// function logsHello(name) {
+//   console.log(`Hello, ${name}!`);
+// }
+
+// logsHello('Chris');
+const sum = 55;
+
+const addsTwoNumbers = (num1, num2) => {
+  const sum = num1 + num2;
+  console.log('inside function' + sum);
+  return sum;
+}
+
+console.log(sum);
+console.log(addsTwoNumbers(2, 1));
